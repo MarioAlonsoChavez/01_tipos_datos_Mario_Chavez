620f591bc6abc20fa571fa02ac2c7659ce295bcf
Alonso Chávez <mario.alonso.chavez.s@gmail.com>
2022-09-10 09:45:44 -0500

    01_tipos_datos_Mario_Chavez

---
+++ intro-typescript-pwa/src/repaso/01_tipos_datos.ts
@@ -0,0 +1,15 @@
+
+
+console.log('Hola Mundo!');
+
+let nombre:string="Mario";
+let cont:number=22;
+let acretida:boolean=false;
+
+let apaterno:number|string="juan";
+
+apaterno=23;
+
+var variable:string="Hola";
+
+const tem=234;
